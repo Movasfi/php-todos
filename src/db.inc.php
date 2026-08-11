b6d116247c92bceb90add496271a4a42fd8ebf3c
Mo21x <mvhajali@st.noorderpoort.nl>
2026-08-11 16:30:08 +0200

    I setted up the project and dependencies

---
+++ src/db.inc.php
@@ -0,0 +1,15 @@
+<?php
+
+$servername = "127.0.0.1";
+$username = "root";
+$password = '';
+$dbname = "todos";
+
+try {
+  $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username);
+  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  echo "Connected successfully";
+} catch(PDOException $e) {
+  echo "Connection failed: " . $e->getMessage();
+}
+
---
+++ src/views/index.php
@@ -0,0 +1,21 @@
+<?php
+include('../db.inc.php');
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../style.css">
+    <title>Todos</title>
+</head>
+
+<body>
+<h1 class="text-2xl text-red-600">gs</h1>
+</body>
+
+</html>
